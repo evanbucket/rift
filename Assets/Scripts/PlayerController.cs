2270137c5d9993ae7913425957c80c945ae6312d
evanbucket <evanbucket@gmail.com>
2023-03-27 15:04:05 -0600

    Added player and player script, along with tilemap and such
    Worked on player movement with reference from the other topdown project I have, with the state machines. I am having issues with jittering with colliding with walls, so Mr. Buckley suggested using rigidbody instead of transform, but the issue is that it adds drag. I genuinely don't know how I made it work in the other project, maybe its in the other project's input settings or something. check it out.
    
    still need to do scene changing and deciding what type of combat I should use

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    enum PlayerState {
+        Idle,
+        Moving,
+    }
+
+    private PlayerState currentState = PlayerState.Idle;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    /* private Animator animator; */
+    private const int SPEED_UNIT = 1000;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        /* animator = GetComponent<Animator>(); */
+        currentState = PlayerState.Idle;
+    }
+
+    void IdleState(Vector2 direction) {
+        if (direction != new Vector2(0,0)){
+            currentState = PlayerState.Moving;
+            sr.color = Color.green;
+            /* Debug.Log("Time to go!"); */
+        }
+    }
+
+    void MoveState(Vector2 direction) {
+        if (direction == new Vector2(0,0)){
+            currentState = PlayerState.Idle;
+            sr.color = Color.red;
+            /* Debug.Log("I'm idle!"); */
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // +1 for right/up, -1 for left/down.
+        // Example: Vector2(1, -1) = diagonal direction right and down.
+        Vector2 direction = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        ).normalized;
+
+        // Take the previous position and add any change to it
+        // in the x and y directions.
+        rb.AddForce(new Vector2(
+            speed * direction.x / SPEED_UNIT,
+            speed * direction.y / SPEED_UNIT
+        ).normalized);
+
+        // If current state is moving, the player is moving and is doing walking animation
+        if (currentState == PlayerState.Moving) {
+            MoveState(direction);
+            /* animator.SetBool("Moving", true); */
+        // If current state is idle, the player is idle, and is not doing the walking animation
+        } else if (currentState == PlayerState.Idle) {
+            IdleState(direction);
+            /* animator.SetBool("Moving", false); */
+        }
+        
+        // Flip sprite
+        if (direction.x < 0) {
+            sr.flipX = true;
+        } else if (direction.x > 0) {
+            sr.flipX = false;
+        }  
+    }
+}
