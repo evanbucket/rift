7b116ed79c79135ea54ca5dc0a196c64ae45d042
evanbucket <evanbucket@gmail.com>
2023-04-10 15:01:54 -0600

    Rooms
    - Expanded the layout for Dungeon1
    - Created the start for Dungeon2
    
    really gotta work on combat soon, because stressing over puzzle designs before the mechanics are even finished is messing me up a LOT

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,15 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
+    
         // If the player has collided with the door, loads a scene based on what tag the door has. 
         if (other.gameObject.tag == "Player") {
             if (this.tag == "Start") {
                 SceneManager.LoadScene("Start", LoadSceneMode.Single);
             } else if (this.tag == "Dungeon1") {
                 SceneManager.LoadScene("Dungeon1", LoadSceneMode.Single);
-            }
+            } /* else if (this.tag == Dungeon [PUT A SYSTEM TO FILL IN THE NUMBER AUTOMATICALLY HERE]) */
         }
     }
 }
