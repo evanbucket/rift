5b14a9b0cf113bb177dff6e3bf43b1d16e36485f
evanbucket <evanbucket@gmail.com>
2023-04-17 15:04:00 -0600

    GOTTA GO I DID ATTACK STUFF IT WORKS KINDA
    https://youtu.be/rwO3TE1G3ag?t=583

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,9 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private const int SPEED_UNIT = 1000;
     public float speed;
+    public GameObject attackPoint;
+    public float radius;
+    public LayerMask enemies;
 
     // Start is called before the first frame update
     void Start()
@@ -27,20 +30,29 @@ public class PlayerController : MonoBehaviour
         currentState = PlayerState.Idle;
     }
 
-    void IdleState(Vector2 direction) {
+    void IdleState(Vector2 direction)
+    {
         if (direction != new Vector2(0,0)){
             currentState = PlayerState.Moving;
             sr.color = Color.green;
-            /* Debug.Log("Time to go!"); */
+            Debug.Log("Time to go!");
         }
     }
 
-    void MoveState(Vector2 direction) {
+    void MoveState(Vector2 direction)
+    {
         if (direction == new Vector2(0,0)){
             currentState = PlayerState.Idle;
             sr.color = Color.red;
-            /* Debug.Log("I'm idle!"); */
+            Debug.Log("I'm idle!");
         }
+    }
+
+    void AttackState()
+    {
+            currentState = PlayerState.Attacking;
+            sr. color = Color.yellow;
+            Debug.Log("ATTACK!");
     }
 
     void FixedUpdate()
@@ -57,6 +69,10 @@ public class PlayerController : MonoBehaviour
             transform.position.y + speed * direction.y / SPEED_UNIT
         );
 
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            currentState = PlayerState.Attacking;
+        }
+
         // If current state is moving, the player is moving and is doing walking animation
         if (currentState == PlayerState.Moving) {
             MoveState(direction);
@@ -65,6 +81,10 @@ public class PlayerController : MonoBehaviour
         } else if (currentState == PlayerState.Idle) {
             IdleState(direction);
             animator.SetBool("isMoving", false);
+        // If the current state is attacking, the player is attacking and doing the attack animation
+        } else if (currentState == PlayerState.Attacking) {
+            AttackState();
+            animator.SetBool("isAttacking", true);
         }
         
         // Flip sprite
@@ -74,4 +94,30 @@ public class PlayerController : MonoBehaviour
             sr.flipX = false;
         }  
     }
+
+    // change to attackTime if it describes this better?
+    public void attack()
+    {
+        Collider2D[] enemy = Physics2D.OverlapCircleAll(attackPoint.transform.position, radius, enemies);
+        
+        foreach (Collider2D enemyGameobject in enemy) {
+            Debug.Log("Hit enemy!");
+        }
+    }
+
+    public void endAttack()
+    {
+        animator.SetBool("isAttacking", false);
+        Debug.Log("Tired.....");
+        currentState = PlayerState.Idle;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attackPoint.transform.position, radius);
+    }
+    // need to lock movement during attack!
+    // need to make attack smaller, as well as change the attack animation!!
+    // 8 class periods left 4/17
+    // pixels per unit make thing smaller
 }
