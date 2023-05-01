52ec3d609d6169aa3e5b63707a5469f1774835c1
evanbucket <evanbucket@gmail.com>
2023-05-01 15:05:25 -0600

    the door USED to work
    I definitely messed something up

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -7,6 +7,20 @@ using UnityEngine.UI;
 public class Door : MonoBehaviour
 {    
     public Text requiredScore;
+    private SpriteRenderer sr;
+    public void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        requiredScore.text = ScoresByScene.requiredScoreByScene["Dungeon1"].ToString("D2");
+    }
+
+    public void Update()
+    {
+        int score = PlayerPrefs.GetInt("score");
+        if (score < ScoresByScene.requiredScoreByScene[this.tag]) {
+            sr.color = Color.grey;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
 
@@ -17,6 +31,7 @@ public class Door : MonoBehaviour
                 SceneManager.LoadScene("Start", LoadSceneMode.Single);
             } else if (this.tag == "Dungeon1" && score >= ScoresByScene.requiredScoreByScene[this.tag]) {
                 SceneManager.LoadScene("Dungeon1", LoadSceneMode.Single);
+                Debug.Log("I can go through the Dungeon1 door!");
                 requiredScore.text = ScoresByScene.requiredScoreByScene["Dungeon2"].ToString("D2");
                 // set score to 0
                 //  PlayerPrefs.SetInt("score", 0);
@@ -25,9 +40,11 @@ public class Door : MonoBehaviour
     }
 }
 
-public static class ScoresByScene {
+public static class ScoresByScene
+{
     public static Dictionary<string, int> requiredScoreByScene = new Dictionary<string, int> {
         // any door of scene "debugroom" has a requirement of a score of 2
+        ["Start"] = 0,
         ["Dungeon1"] = 2,
         ["Dungeon2"] = 4,
     };
